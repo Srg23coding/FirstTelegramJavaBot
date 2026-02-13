e96b60b81bd933bc53c32b5b6edeb6a297235d5e
Srg231 <sejrz2325@mail.ru>
2026-02-13 11:12:42 +0300

    MyCommit

--- src/main/java/javaprojects/firsttgbot/firsttelegramjavabot/MyTelegramBot.java
+++ src/main/java/javaprojects/firsttgbot/firsttelegramjavabot/MyTelegramBot.java
@@ -1,4 +1,24 @@
 package javaprojects.firsttgbot.firsttelegramjavabot;
 
-public class MyTelegramBot {
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+
+@Component
+public class MyTelegramBot implements SpringLongPollingBot {
+    private final UpdateConsumer updateConsumer;
+
+    public MyTelegramBot(UpdateConsumer updateConsumer) {
+        this.updateConsumer = updateConsumer;
+    }
+
+    @Override
+    public String getBotToken() {
+        return "8426677991:AAE1kpo2VEALUoaEHLDQ0kOzK39W-V0nd5g";
+    }
+
+    @Override
+    public LongPollingUpdateConsumer getUpdatesConsumer() {
+        return updateConsumer;
+    }
 }
--- src/main/java/javaprojects/firsttgbot/firsttelegramjavabot/UpdateConsumer.java
+++ src/main/java/javaprojects/firsttgbot/firsttelegramjavabot/UpdateConsumer.java
@@ -1,4 +1,142 @@
 package javaprojects.firsttgbot.firsttelegramjavabot;
 
-public class UpdateConsumer {
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
+public class UpdateConsumer implements LongPollingSingleThreadUpdateConsumer {
+    private TelegramClient telegramClient;
+
+    public UpdateConsumer() {
+        this.telegramClient = new OkHttpTelegramClient("8426677991:AAE1kpo2VEALUoaEHLDQ0kOzK39W-V0nd5g");
+    }
+
+    @SneakyThrows
+    @Override
+    public void consume(Update update) {
+        if (update.hasMessage()){
+            String messageText = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
+
+            if (messageText.equals("/start")){
+                sendMainMenu(chatId);
+            } else {
+                sendMessage(chatId, "Я вас не понимаю");
+            }
+        } else if (update.hasCallbackQuery()){
+            handleCallBackQuery(update.getCallbackQuery());
+        }
+    }
+
+    private void handleCallBackQuery(CallbackQuery callbackQuery) {
+        var data = callbackQuery.getData();
+        var chatId = callbackQuery.getFrom().getId();
+        var user = callbackQuery.getFrom();
+        switch (data){
+            case "my_name" -> sendMyName(chatId, user);
+            case "random" -> sendRandom(chatId);
+            case "long_process" -> sendImage(chatId);
+            default -> sendMessage(chatId, "Неизвестная команда");
+        }
+    }
+
+    @SneakyThrows
+    private void sendMessage(Long chatId, String messageText) {
+        SendMessage message = SendMessage.builder()
+                .text(messageText)
+                .chatId(chatId)
+                .build();
+
+        telegramClient.execute(message);
+    }
+
+    private void sendImage(Long chatId) {
+        sendMessage(chatId, "Запустили загрузку картинки");
+        new Thread(() -> {
+            var imageUrl = "https://avatars.mds.yandex.net/get-autoru-vos/4479447/1dd0eab102b1310d051c159978d7ba9d/1200x900";
+            try {
+                URL url = new URL(imageUrl);
+                var inputStream = url.openStream();
+
+                SendPhoto sendPhoto = SendPhoto.builder()
+                        .chatId(chatId)
+                        .photo(new InputFile(inputStream, "random.jpg"))
+                        .caption("Ваша случайная картинка")
+                        .build();
+
+
+                telegramClient.execute(sendPhoto);
+                inputStream.close();
+            } catch (TelegramApiException | IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
+    private void sendRandom(Long chatId) {
+        var randomInt = ThreadLocalRandom.current().nextInt();
+        sendMessage(chatId, "Ваше рандомное число: " + randomInt);
+    }
+
+    private void sendMyName(Long chatId, User user) {
+        var text = "Привет! \n\nВас зовут: %s\nВаш ник: @%s"
+                .formatted(
+                  user.getFirstName() + " " + user.getLastName(),
+                  user.getUserName()
+                );
+        sendMessage(chatId, text);
+    }
+
+    @SneakyThrows
+    public void sendMainMenu(Long chatId){
+        SendMessage message = SendMessage.builder()
+                .text("Добро пожаловать! Выберите действие:")
+                .chatId(chatId)
+                .build();
+
+        var button1 = InlineKeyboardButton.builder()
+                .text("Как меня зовут?")
+                .callbackData("my_name")
+                .build();
+
+        var button2 = InlineKeyboardButton.builder()
+                .text("Случайное число")
+                .callbackData("random")
+                .build();
+
+        var button3 = InlineKeyboardButton.builder()
+                .text("Долгий процесс")
+                .callbackData("long_process")
+                .build();
+
+        List<InlineKeyboardRow> keyboardRows = List.of(
+                new InlineKeyboardRow(button1),
+                new InlineKeyboardRow(button2),
+                new InlineKeyboardRow(button3)
+        );
+
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup(keyboardRows);
+
+        message.setReplyMarkup(markup);
+        telegramClient.execute(message);
+    }
 }
